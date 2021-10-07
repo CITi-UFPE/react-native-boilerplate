aaf48c9ec537b8640795e751b1432c5f3aaef678
IgrPhillipe <IgrPhillipe@gmail.com>
2021-10-07 10:32:23 -0300

    Fix eslint

--- src/components/Button/Button.stories.tsx
+++ src/components/Button/Button.stories.tsx
@@ -11,6 +11,7 @@ export default {
   },
 } as ComponentMeta<typeof Button>;
 
+// eslint-disable-next-line react/jsx-props-no-spreading
 const Template: ComponentStory<typeof Button> = (args: any) => <Button {...args} />;
 
 export const Primary = Template.bind({});
--- src/components/StoryBookTestComponent/StoryBookTesteComponent.stories.tsx
+++
@@ -1,5 +0,0 @@
-import { storiesOf } from '@storybook/react-native';
-
-import StoryBookTestComponent from './index';
-
-storiesOf('Storybook test Component', module).add('example', () => <StoryBookTestComponent />);
